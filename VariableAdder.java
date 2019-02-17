05f9e6b65f2919c2ce8fe9ee587b09fd0370fd21
patrick <kabejames@>
2019-02-16 21:30:07 -0600

    Homework4

---
+++ VariableAdder.java
@@ -0,0 +1,17 @@
+package edu.dmacc.codedsm.hw4;
+
+public class VariableAdder {
+    final static int MY_CAR = 5;
+    static int mum_car = 2;
+
+
+    public static void main(String[] args) {
+
+        int brother_car = 6;
+
+        int result = MY_CAR + mum_car + brother_car;
+
+        System.out.println(result);
+
+    }
+}
